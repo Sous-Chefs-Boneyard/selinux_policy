4d46d117033229cae35d8b13e43ae4cb19ded768
Nitz <nitz.raz@gmail.com>
2014-12-04 16:30:47 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'selinuxpolicy'
+maintainer       'Backslasher'
+maintainer_email 'nitz.raz@gmail.com'
+license          'GPL v2'
+description      'Manages SELinux policy components'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
---
+++ providers/boolean.rb
@@ -0,0 +1,24 @@
+# Support whyrun
+def whyrun_supported?
+  true
+end
+
+# Set for now, without persisting
+action :set do
+  set_sebool(false)
+end
+
+# Set and persist
+action :setpersist do
+  set_sebool(true)
+end
+
+def set_sebool(persist=false)
+  persist_string= persist ? '-P ':''
+  new_value= new_resource.state ? 'on' : 'off'
+  e = execute "selinux-setbool-#{new_resource.name}-#{new_value}" do
+    command "/usr/sbin/setsebool #{persist_string} #{new_resource.name} #{new_value}"
+    not_if "/usr/sbin/getsebool #{new_resource.name} | grep '#{new_value}$' >/dev/null" if !new_resource.force
+  end
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
