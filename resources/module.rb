1a3cff1e45930462f609d7db8e70806e564dcc0b
Nitz <nitz.raz@gmail.com>
2014-12-15 17:25:44 +0200

    Added selinux_policy_module

---
+++ resources/module.rb
@@ -0,0 +1,8 @@
+# A resource for managing SE modules
+
+actions :deploy, :remove
+default_action :deploy
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :content, :kind_of => String, :default => nil
+attribute :force, :kind_of => [ TrueClass, FalseClass ], :default => false
