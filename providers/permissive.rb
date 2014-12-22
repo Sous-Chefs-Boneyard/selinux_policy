d9819032c9ab408880fed14f3f9ca80a58144927
Nitz <nitz.raz@gmail.com>
2014-12-22 15:28:07 +0200

    Added selinux_policy_permissive

---
+++ providers/permissive.rb
@@ -0,0 +1,24 @@
+# Support whyrun
+def whyrun_supported?
+  true
+end
+
+# Create if doesn't exist, do not touch if port is already registered (even under different type)
+action :add do
+  e = execute "selinux-permissive-#{new_resource.name}-add" do
+    command "/usr/sbin/semanage permissive -a '#{new_resource.name}'"
+    not_if  "/usr/sbin/semanage permissive -l | grep  '^#{new_resource.name}$'"
+  end
+
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
+
+# Delete if exists
+action :delete do
+  e = execute "selinux-port-#{new_resource.name}-delete" do
+    command "/usr/sbin/semanage permissive -d '#{new_resource.name}'"
+    not_if  "/usr/sbin/semanage permissive -l | grep  '^#{new_resource.name}$'"
+  end
+
+  new_resource.updated_by_last_action(e.updated_by_last_action?)
+end
