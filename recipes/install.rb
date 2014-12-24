a372b1424f8b6b489ac6c475c163757ba9c19657
Nitz <nitz.raz@gmail.com>
2014-12-24 17:50:05 +0200

    Fixed terrible syntax in install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,11 +6,13 @@
 #
 # GPLv2
 #
-case platform_family
-#policycoreutils-python
-when "debian" pkgs = [ 'policycoreutils', 'selinux-policy-dev', 'make' ]
-when "rhel" pkgs = [ 'policycoreutils-python', 'selinux-policy', 'make' ]
-else raise 'Uknown distro, cannot determine required package names'
+case node[:platform_family]
+  when "debian"
+    pkgs = [ 'policycoreutils', 'selinux-policy-dev', 'make' ]
+  when "rhel"
+    pkgs = [ 'policycoreutils-python', 'selinux-policy', 'make' ]
+  else
+    raise 'Uknown distro, cannot determine required package names'
 end
 
 pkgs.each{|p|package p}
