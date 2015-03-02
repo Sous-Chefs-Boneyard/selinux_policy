7bfe602e2f83ea8fdf8048c05c7d6bc36e1d6865
Jörg Herzinger <joerg.herzinger@bytesource.net>
2015-03-02 17:31:21 +0100

    Add fcontext resource to manage file contexts.

---
+++ resources/fcontext.rb
@@ -0,0 +1,7 @@
+# Manages file specs in SELinux
+# See http://docs.fedoraproject.org/en-US/Fedora/13/html/SELinux_FAQ/index.html#id3715134
+
+actions :add, :delete, :modify, :addormodify
+default_action :addormodify
+attribute :file_spec, :kind_of => String, :name_attribute => true
+attribute :secontext, :kind_of => String
