a8f4d12e77d70f45d9af965fcdfb5ad5a721548c
Nitz <nitz.raz@gmail.com>
2015-03-03 12:26:42 +0200

    Switched if statements to only_if
    Fixes FC023

--- providers/module.rb
+++ providers/module.rb
@@ -7,28 +7,27 @@ use_inline_resources
 
 # Compile and deploy module (also upgrade)
 action :deploy do
-  if use_selinux then
-    filename="semodule-#{new_resource.name}"
-    f = file "#{Chef::Config[:file_cache_path]}/#{filename}.te" do
-      content new_resource.content
-      # notifies :run, "execute[semodule-deploy-#{new_resource.name}]"
-    end
+  filename="semodule-#{new_resource.name}"
+  f = file "#{Chef::Config[:file_cache_path]}/#{filename}.te" do
+    content new_resource.content
+    # notifies :run, "execute[semodule-deploy-#{new_resource.name}]"
+    only_if {use_selinux}
+  end
 
-    e=execute "semodule-deploy-#{new_resource.name}" do
-      # action ( new_resource.force ? :run : :nothing )
-      command "/usr/bin/make -f /usr/share/selinux/devel/Makefile #{filename}.pp && /usr/sbin/semodule -i #{filename}.pp"
-      only_if {f.updated_by_last_action? or new_resource.force or (shell_out("/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'").stdout == '')}
-      cwd Chef::Config[:file_cache_path]
-    end
+  e=execute "semodule-deploy-#{new_resource.name}" do
+    # action ( new_resource.force ? :run : :nothing )
+    command "/usr/bin/make -f /usr/share/selinux/devel/Makefile #{filename}.pp && /usr/sbin/semodule -i #{filename}.pp"
+    only_if {f.updated_by_last_action? or new_resource.force or (shell_out("/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'").stdout == '')}
+    cwd Chef::Config[:file_cache_path]
+    only_if {use_selinux}
   end
 end
 
 # remove module
 action :remove do
-  if use_selinux then
-    execute "semodule-remove-#{new_resource.name}" do
-      command "/usr/sbin/semodule -r #{new_resource.name}"
-      only_if "/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'"
-    end
+  execute "semodule-remove-#{new_resource.name}" do
+    command "/usr/sbin/semodule -r #{new_resource.name}"
+    only_if "/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'"
+    only_if {use_selinux}
   end
 end
--- providers/port.rb
+++ providers/port.rb
@@ -7,36 +7,32 @@ use_inline_resources
 
 # Create if doesn't exist, do not touch if port is already registered (even under different type)
 action :add do
-  if use_selinux then
-    execute "selinux-port-#{new_resource.port}-add" do
-      command "/usr/sbin/semanage port -a -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
-      not_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
-    end
+  execute "selinux-port-#{new_resource.port}-add" do
+    command "/usr/sbin/semanage port -a -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
+    not_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
+    only_if {use_selinux}
   end
 end
 
 # Delete if exists
 action :delete do
-  if use_selinux then
-    execute "selinux-port-#{new_resource.port}-delete" do
-      command "/usr/sbin/semanage port -d -p #{new_resource.protocol} #{new_resource.port}"
-      only_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
-    end
+  execute "selinux-port-#{new_resource.port}-delete" do
+    command "/usr/sbin/semanage port -d -p #{new_resource.protocol} #{new_resource.port}"
+    only_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
+    only_if {use_selinux}
   end
 end
 
 action :modify do
-  if use_selinux then
-    execute "selinux-port-#{new_resource.port}-modify" do
-      command "/usr/sbin/semanage port -m -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
-    end
+  execute "selinux-port-#{new_resource.port}-modify" do
+    command "/usr/sbin/semanage port -m -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}"
+    only_if {use_selinux}
   end
 end
 
 action :addormodify do
-  if use_selinux then
-    execute "selinux-port-#{new_resource.port}-addormodify" do
-      command <<-EOT
+  execute "selinux-port-#{new_resource.port}-addormodify" do
+    command <<-EOT
     if /usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null; then
       /usr/sbin/semanage port -m -t #{new_resource.secontext} -p #{new_resource.protocol} #{new_resource.port}
     else
@@ -44,6 +40,6 @@ action :addormodify do
     fi
     EOT
     not_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.secontext}\\s+#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
-    end
+    only_if {use_selinux}
   end
 end
