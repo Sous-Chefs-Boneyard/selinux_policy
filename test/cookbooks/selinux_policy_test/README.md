8c0b5ca8db1a8f2eb341a331b1e3fc05c15d1f43
Kieren Evans <git@kle.me>
2015-04-12 15:20:44 +0100

    Add test kitchen support for chef 11 and 12.

---
+++ test/cookbooks/selinux_policy_test/README.md
@@ -0,0 +1,4 @@
+# selinux_policy_test
+
+TODO: Enter the cookbook description here.
+
---
+++ test/cookbooks/selinux_policy_test/metadata.rb
@@ -0,0 +1,10 @@
+name             'selinux_policy_test'
+maintainer       'Kieren Evans'
+maintainer_email 'git@kle.me'
+license          'GPL v2'
+description      'Wrapper cookbook to test selinux_policy LWRPs'
+long_description 'Wrapper cookbook to test selinux_policy LWRPs'
+version          '0.1.0'
+
+depends          'selinux', '~> 0.9.0'
+depends          'selinux_policy'
