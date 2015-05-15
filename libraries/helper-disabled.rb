d60ab395dbe354cfc5fc5007e9c140c7a56d31c5
James Le Cuirot <james.le-cuirot@yakara.com>
2015-05-15 22:29:27 +0100

    Move helpers into a cookbook-specific module
    Putting them directly in Chef::Provider is bad practice.
    Also allows recipes in other cookbooks to use them.

--- libraries/helper-disabled.rb
+++ libraries/helper-disabled.rb
@@ -6,15 +6,17 @@ require 'chef/mixin/shell_out'
 include Chef::Mixin::ShellOut
 
 class Chef
-  class Provider
-    # Checks if SELinux is disabled and whether we're allowed to run when disabled
-    def use_selinux
-      selinux_disabled= (shell_out!("getenforce").stdout =~ /disabled/i)
-      allowed_disabled=node['selinux_policy']['allow_disabled']
-      # return false only when SELinux is disabled and it's allowed
-      return_val=!(selinux_disabled && allowed_disabled)
-      Chef::Log.warn('SELinux is disabled, skipping') if !return_val
-      return return_val
+  module SELinuxPolicy
+    module Helpers
+      # Checks if SELinux is disabled and whether we're allowed to run when disabled
+      def use_selinux
+        selinux_disabled= (shell_out!("getenforce").stdout =~ /disabled/i)
+        allowed_disabled=node['selinux_policy']['allow_disabled']
+        # return false only when SELinux is disabled and it's allowed
+        return_val=!(selinux_disabled && allowed_disabled)
+        Chef::Log.warn('SELinux is disabled, skipping') if !return_val
+        return return_val
+      end
     end
   end
 end
