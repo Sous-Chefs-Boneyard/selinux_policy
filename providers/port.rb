9f3b669a77e6e5c92468cdb795a04bcc7e31ab65
Edward Quick <edward.quick@barclays.com>
2015-07-16 16:02:56 +0100

    updated regex on delete

--- providers/port.rb
+++ providers/port.rb
@@ -20,7 +20,7 @@ end
 action :delete do
   execute "selinux-port-#{new_resource.port}-delete" do
     command "/usr/sbin/semanage port -d -p #{new_resource.protocol} #{new_resource.port}"
-    only_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.protocol}\\s+#{new_resource.port}'>/dev/null"
+    only_if "/usr/sbin/semanage port -l | grep -P '#{new_resource.secontext}\\s+#{new_resource.protocol}\\s*(\\s\\d+,)*\\s*#{new_resource.port}(,|$)'>/dev/null"
     only_if {use_selinux}
   end
 end
