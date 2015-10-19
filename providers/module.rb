67f8899fec214a9a440aa76b6d39286cd08f5708
Nitz <nitz.raz@gmail.com>
2015-10-19 16:35:29 +0300

    Moved detection to module_defined

--- providers/module.rb
+++ providers/module.rb
@@ -5,6 +5,10 @@ def whyrun_supported?
   true
 end
 
+def module_defined(name)
+  "/usr/sbin/semodule -l | grep -w '^#{name}'"
+end
+
 use_inline_resources
 
 # Compile and deploy module (also upgrade)
@@ -19,7 +23,7 @@ action :deploy do
   e=execute "semodule-deploy-#{new_resource.name}" do
     # action ( new_resource.force ? :run : :nothing )
     command "/usr/bin/make -f /usr/share/selinux/devel/Makefile #{filename}.pp && /usr/sbin/semodule -i #{filename}.pp"
-    only_if {f.updated_by_last_action? or new_resource.force or (shell_out("/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'").stdout == '')}
+    only_if {f.updated_by_last_action? or new_resource.force or (shell_out(module_defined(name)).stdout == '')}
     cwd Chef::Config[:file_cache_path]
     only_if {use_selinux}
   end
@@ -29,7 +33,7 @@ end
 action :remove do
   execute "semodule-remove-#{new_resource.name}" do
     command "/usr/sbin/semodule -r #{new_resource.name}"
-    only_if "/usr/sbin/semodule -l | grep '^#{new_resource.name}\\s'"
+    only_if module_defined(new_resource.name)
     only_if {use_selinux}
   end
 end
