4454adb8ab876a38be006aa5fdf82448e339d3eb
Nitz <nitz.raz@gmail.com>
2015-10-19 15:53:17 +0300

    Created basic test for fcontext

---
+++ test/cookbooks/selinux_policy_test/recipes/fcontext.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: selinux_policy_test
+# Recipe:: fcontext
+#
+# Copyright 2015, BackSlasher
+#
+# GPLv2
+
+# Define a single fcontext
+ruby_block 'fail-mismatch' do
+  action :nothing
+  block do
+    raise 'Fail block was invoked'
+  end
+end
+
+selinux_policy_fcontext '/tmp/test' do
+  secontext 'httpd_tmp_t'
+end
+
+directory '/tmp/test'
+
+# Fail if dir isn't actually set
+execute 'true' do
+  not_if 'stat -c %C /tmp/test | grep httpd_tmp_t'
+  notifies :run, 'ruby_block[fail-mismatch]', :immediate
+end
