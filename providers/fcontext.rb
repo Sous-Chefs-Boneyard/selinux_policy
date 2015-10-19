5443c024ef057076110fe6a18a8008180f8c9a0b
Nitz <nitz.raz@gmail.com>
2015-10-19 16:13:19 +0300

    Removed lazy command
    helps nothing

--- providers/fcontext.rb
+++ providers/fcontext.rb
@@ -11,7 +11,7 @@ use_inline_resources
 action :add do
   escaped_file_spec = Regexp.escape(new_resource.file_spec)
   execute "selinux-fcontext-#{new_resource.secontext}-add" do
-    command lazy { "/usr/sbin/semanage fcontext -a -t #{new_resource.secontext} '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}" }
+    command "/usr/sbin/semanage fcontext -a -t #{new_resource.secontext} '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}"
     not_if "/usr/sbin/semanage fcontext -l | grep -P '#{escaped_file_spec}\s.*\ssystem_u:object_r:#{new_resource.secontext}:s0'>/dev/null"
     only_if {use_selinux}
   end
@@ -21,7 +21,7 @@ end
 action :delete do
   escaped_file_spec = Regexp.escape(new_resource.file_spec)
   execute "selinux-fcontext-#{new_resource.secontext}-delete" do
-    command lazy { "/usr/sbin/semanage fcontext -d '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}" }
+    command "/usr/sbin/semanage fcontext -d '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}"
     only_if "/usr/sbin/semanage fcontext -l | grep -P '#{escaped_file_spec}\s.*\ssystem_u:object_r:#{new_resource.secontext}:s0'>/dev/null"
     only_if {use_selinux}
   end
@@ -29,7 +29,7 @@ end
 
 action :modify do
   execute "selinux-fcontext-#{new_resource.secontext}-modify" do
-    command lazy { "/usr/sbin/semanage fcontext -m -t #{new_resource.secontext} '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}" }
+    command "/usr/sbin/semanage fcontext -m -t #{new_resource.secontext} '#{new_resource.file_spec}' && #{restorecon(new_resource.file_spec)}"
     only_if {use_selinux}
   end
 end
@@ -37,14 +37,14 @@ end
 action :addormodify do
   escaped_file_spec = Regexp.escape(new_resource.file_spec)
   execute "selinux-fcontext-#{new_resource.secontext}-addormodify" do
-    command lazy { "
+    command "
     if /usr/sbin/semanage fcontext -l | grep -P '#{escaped_file_spec}\s.*'>/dev/null; then
       /usr/sbin/semanage fcontext -m -t #{new_resource.secontext} '#{new_resource.file_spec}' &&
       #{restorecon(new_resource.file_spec)}
     else
       /usr/sbin/semanage fcontext -a -t #{new_resource.secontext} '#{new_resource.file_spec}' &&
       #{restorecon(new_resource.file_spec)}
-    fi" }
+    fi"
     not_if "/usr/sbin/semanage fcontext -l | grep -P '#{escaped_file_spec}\s.*\ssystem_u:object_r:#{new_resource.secontext}:s0'>/dev/null"
     only_if {use_selinux}
   end
