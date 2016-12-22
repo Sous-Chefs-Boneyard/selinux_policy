12bc375b3817725fd4f28b1f7e8b4abcb765363b
Grant Ridder <shortdudey123@gmail.com>
2016-12-22 01:09:49 -0800

    Cleanup cookstyle offenses

--- providers/boolean.rb
+++ providers/boolean.rb
@@ -9,18 +9,18 @@ use_inline_resources
 
 # Set for now, without persisting
 action :set do
-  set_sebool(false)
+  sebool(false)
 end
 
 # Set and persist
 action :setpersist do
-  set_sebool(true)
+  sebool(true)
 end
 
-def set_sebool(persist = false)
+def sebool(persist = false)
   persist_string = persist ? '-P ' :  ''
   new_value = new_resource.value ? 'on' : 'off'
-  e = execute "selinux-setbool-#{new_resource.name}-#{new_value}" do
+  execute "selinux-setbool-#{new_resource.name}-#{new_value}" do
     command "/usr/sbin/setsebool #{persist_string} #{new_resource.name} #{new_value}"
     not_if "/usr/sbin/getsebool #{new_resource.name} | grep '#{new_value}$' >/dev/null" unless new_resource.force
     only_if { use_selinux }
