fafc2cc2c01992224e2d357c9f5b23f72ff6f979
Tim Smith <tsmith@chef.io>
2017-02-12 11:10:20 -0800

    Require Chef 12+ and use compat_resource
    This gets us Chef 13 compatibility
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,19 +6,15 @@ description      'Manages SELinux policy components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.1'
 
-attribute 'selinux_policy',
-          display_name: 'SELinux Policy',
-          description: 'Hash of SELinux policy related attributes',
-          type: 'hash'
-
 supports 'redhat'
 supports 'centos'
 supports 'fedora'
 supports 'ubuntu'
 supports 'debian'
+supports 'amazon'
 
-depends 'yum', '~> 4.0'
+depends 'compat_resource', '>= 12.16.3'
 
-source_url 'https://github.com/sous-chefs/selinux_policy' if respond_to?(:source_url)
-issues_url 'https://github.com/sous-chefs/selinux_policy/issues' if respond_to?(:issues_url)
-chef_version '>= 11.0' if respond_to?(:chef_version)
+source_url 'https://github.com/sous-chefs/selinux_policy'
+issues_url 'https://github.com/sous-chefs/selinux_policy/issues'
+chef_version '>= 12.1' if respond_to?(:chef_version)
