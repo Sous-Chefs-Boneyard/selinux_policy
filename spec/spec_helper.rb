30fc1c6917530303554fcbb8d143b8f702b90deb
Dan Webb <dan.webb@damacus.io>
2018-03-05 14:56:02 +0000

    Move SoloRunner hackery into spec_helper. Update platform version.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+module ChefSpec
+  class SoloRunner
+    def converge_dsl(*recipes, &block)
+      cookbook_name = 'imaginary'
+      recipe_name = 'temp'
+      converge(*recipes) do
+        recipe = Chef::Recipe.new(cookbook_name, recipe_name, @run_context)
+        recipe.instance_eval(&block)
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.color = true
   config.tty = true
