43544e611346248b022eeb4dafc53ff658aa02e7
David <Techcadia@users.noreply.github.com>
2019-11-15 22:33:28 -0800

    [Fix] use_selinux refactor in #99 caused breakage in resource/permissive.rb (#106)
    * [Fix] use_selinux refactor
    
    * Markdown style fixes
    
    * Cookstyle fixes
    
    * Adds final job step

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,10 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Manages SELinux policy components'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/sous-chefs/selinux_policy'
 issues_url       'https://github.com/sous-chefs/selinux_policy/issues'
 chef_version     '>= 13.0'
-version          '2.3.4'
+version          '2.3.5'
 
 supports 'redhat'
 supports 'centos'
